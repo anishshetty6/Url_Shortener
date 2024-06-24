d5dfa68577f38e10f86e0ec173f2a34d1710333f
anishshetyy6 <anishshetty2708@gmail.com>
2024-06-24 15:39:23 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
 import { useState } from 'react'
-import './App.css'
+import AppLayout from './layouts/AppLayout'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import Auth from './pages/Auth'
+import Dashboard from './pages/Dashboard'
+import Link from './pages/Link'
+import RedirectLink from './pages/RedirectLink'
+
+const router=createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:'/',
+        element:<LandingPage/>
+      },
+      {
+        path:'/dash',
+        element:<Dashboard/>
+      },
+      {
+        path:'/auth',
+        element:<Auth/>
+      },
+      {
+        path:'/link/:id',
+        element:<Link/>
+      },
+      {
+        path:'/:id',
+        element:<RedirectLink/>
+      },
+    ]
+  }
+])
 
 function App() {
 
   return (
     <>
-      <h2 className='text-3xl'>Hello</h2>
+      <RouterProvider router={router}/>
     </>
   )
 }
