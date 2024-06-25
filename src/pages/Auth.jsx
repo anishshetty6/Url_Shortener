0b3dbe492179371f2228ed84ab167c24471ef285
anishshetyy6 <anishshetty2708@gmail.com>
2024-06-25 17:31:00 +0530

    initial auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 const Auth = () => {
+
+  const [searchParams] = useSearchParams();
+
   return (
-    <div>Auth</div>
+    <div className=' mt-24 flex flex-col items-center gap-10'>
+      <h1 className='text-4xl font-extrabold'>
+        {searchParams.get('createNew')
+          ? "Wait!Let's get you logged in first 😊"
+          :
+          <Tabs defaultValue="Login" className="w-[400px]">
+            <TabsList className='grid w-full grid-cols-2'>
+              <TabsTrigger value="Login">Login</TabsTrigger>
+              <TabsTrigger value="Signup">Signup</TabsTrigger>
+            </TabsList>
+            <TabsContent value="Login">Login Component</TabsContent>
+            <TabsContent value="Signup">Signup component</TabsContent>
+          </Tabs>
+
+        }
+      </h1>
+
+    </div>
   )
 }
 
