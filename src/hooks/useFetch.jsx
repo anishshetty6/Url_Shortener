acdefef87dcec68eea934a31ed1a0e2fae5b50a7
anishshetyy6 <anishshetty2708@gmail.com>
2024-06-27 16:15:10 +0530

    custom hook for fetching login details

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+import React from 'react'
+
+const useFetch = (cb,options={}) => {
+    const [data,setData]=useState(null);
+    const[loading,setLoading]=useState(null);
+    const[error,setError]=useState(null);
+
+    const fn=async(...args)=>{
+        setLoading(true);
+        setError(null);
+        try{
+            const response=await cb(options,...args);
+            setData(response);
+            setError(null);
+        } catch(error){
+            setError(error);
+        }finally{
+            setLoading(false);
+        }
+    }
+
+    return [data,fn,loading,error];
+}
+
+export default useFetch
