e659d9ed64f6557beaaf14eea2dd44bf96fce97b
anishshetyy6 <anishshetty2708@gmail.com>
2024-06-28 23:47:57 +0530

    auth changes

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
         
