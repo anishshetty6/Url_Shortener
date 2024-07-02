a0445b7c69c1cded3fda4127bb899da8e169ade9
anishshetyy6 <anishshetty2708@gmail.com>
2024-07-03 01:10:57 +0530

    urls api

---
+++ src/db/apiUrls.js
@@ -0,0 +1,12 @@
+import supabase, { supabaseUrl } from "./supabase";
+
+export async function getUrls(user_id) {
+    const { data, error } = await supabase
+        .from("urls")
+        .select("*")
+        .eq("user_id", user_id);
+
+    if (error) throw new Error(error.message)
+
+    return data
+}
