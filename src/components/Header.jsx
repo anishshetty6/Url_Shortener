090ff7179bacea803fb882d578a998b876d307a5
anishshetyy6 <anishshetty2708@gmail.com>
2024-07-02 21:28:03 +0530

    logout function added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,14 +11,18 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { LinkIcon, LogOut } from 'lucide-react'
-
-
-
+import { urlState } from '@/context'
+import useFetch from '@/hooks/useFetch'
+import { logout } from '@/db/apiAuth'
 
 const Header = () => {
 
     const navigate = useNavigate()
-    const user = false;
+
+    const { user, fetchUser } = urlState()
+
+    const { loading, fn: fnLogout } = useFetch(logout);
+
     return (
         <nav className='flex justify-between items-center p-4'>
             <Link to='/'>
@@ -31,27 +35,32 @@ const Header = () => {
                         <DropdownMenu>
                             <DropdownMenuTrigger className='w-10 rounded-full overflow-hidden'>
                                 <Avatar>
-                                    <AvatarImage src="/scott.png" />
+                                    <AvatarImage src={user?.user_metadata?.profile_pic} alt="dp" />
                                     <AvatarFallback>AS</AvatarFallback>
                                 </Avatar>
                             </DropdownMenuTrigger >
                             <DropdownMenuContent>
-                                <DropdownMenuLabel>Anish Shetty</DropdownMenuLabel>
+                                <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
                                 <DropdownMenuSeparator />
                                 <DropdownMenuItem>
                                     <LinkIcon className='mr-2 h-4 w-4' />
                                     <span> My Links</span>
                                 </DropdownMenuItem>
-                                <DropdownMenuItem className='text-red-400'>
+                                <DropdownMenuItem className='text-red-400'
+                                onClick={() => {
+                                    fnLogout().then(() => {
+                                        navigate('/auth')
+                                    })
+                                }}>
                                     <LogOut className='mr-2 h-4 w-4' />
-                                    <span>Logout</span>
+                                    <span >Logout</span>
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
-
                     )
                 }
             </div>
+            {loading && <div>Loading...</div>}
         </nav>
     )
 }
