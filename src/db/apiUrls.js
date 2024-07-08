d0e2a62c161cc60241d945b8b338a0de7d7d0a83
anishshetyy6 <anishshetty2708@gmail.com>
2024-07-09 01:32:35 +0530

    api for clicks information

--- src/db/apiUrls.js
+++ src/db/apiUrls.js
@@ -1,3 +1,4 @@
+import { UAParser } from "ua-parser-js";
 import supabase, { supabaseUrl } from "./supabase";
 
 export async function getUrls(user_id) {
@@ -59,4 +60,42 @@ export async function createUrl({ title, longUrl, customUrl, user_id }, qrcode) 
     }
 
     return data;
+}
+
+export async function getLongUrl(id) {
+    const { data, error } = await supabase
+        .from("urls")
+        .select("id,original_url")
+        .or(`short_url.eq.${id},custom_url.eq.${id}`)
+        .signle();
+
+    if (error) {
+        console.error(error.message)
+        throw new Error("Error fetching the short link");
+    }
+
+    return data;
+}
+
+const parser= new UAParser();
+
+export const storeClicks=async({id,originalUrl})=>{
+    try{
+        const res=parser.getResult();
+        const device = res.type|| "desktop";
+
+        const response=await fetch("https://ipapi.co/json");
+        const {city,country_name:country}=await response.json();
+
+        await supabase.from("clicks").insert({
+            url_id:id,
+            city:city,
+            country:country,
+            device:device
+        });
+        window.location.href=originalUrl;
+    }
+    catch(error){
+        console.log("Error recording click info",error);
+    }
 }
